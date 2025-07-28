bb19b3828a682ab45c625ed95de802df224f12cf
BlakeBr0 <luasmrks@gmail.com>
2025-07-28 13:31:37 -0500

    Fix Furnace screen background texture location closes #811

--- src/main/java/com/blakebr0/mysticalagriculture/client/screen/EssenceFurnaceScreen.java
+++ src/main/java/com/blakebr0/mysticalagriculture/client/screen/EssenceFurnaceScreen.java
@@ -12,7 +12,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class EssenceFurnaceScreen extends BaseContainerScreen<EssenceFurnaceContainer> {
-    private static final ResourceLocation BACKGROUND = MysticalAgriculture.resource("textures/gui/reprocessor.png");
+    private static final ResourceLocation BACKGROUND = MysticalAgriculture.resource("textures/gui/furnace.png");
     private EssenceFurnaceTileEntity tile;
 
     public EssenceFurnaceScreen(EssenceFurnaceContainer container, Inventory inv, Component title) {
