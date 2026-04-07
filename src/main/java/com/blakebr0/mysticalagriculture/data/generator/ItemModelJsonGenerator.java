c8a5dd586ffd52c983a034955560109399294999
BlakeBr0 <luasmrks@gmail.com>
2026-04-06 21:29:11 -0500

    Create client client item JSONs for all items

--- src/main/java/com/blakebr0/mysticalagriculture/data/generator/ItemModelJsonGenerator.java
+++ src/main/java/com/blakebr0/mysticalagriculture/data/generator/ItemModelJsonGenerator.java
@@ -6,10 +6,12 @@ import com.blakebr0.mysticalagriculture.registry.CropRegistry;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
+import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelLocationUtils;
 import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.client.data.models.model.TextureMapping;
 import net.minecraft.client.data.models.model.TextureSlot;
+import net.minecraft.client.renderer.item.ClientItem;
 import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.core.Holder;
 import net.minecraft.data.PackOutput;
@@ -28,16 +30,36 @@ public class ItemModelJsonGenerator extends ModelProvider {
     protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
         for (var crop : CropRegistry.getInstance().getCrops()) {
             if (crop.shouldRegisterEssenceItem()) {
+                var item = crop.getEssenceItem();
+
+                itemModels.itemModelOutput.register(
+                        item,
+                        new ClientItem(
+                                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(item)),
+                                ClientItem.Properties.DEFAULT
+                        )
+                );
+
                 ModelTemplates.FLAT_ITEM.create(
-                        crop.getEssenceItem(),
+                        item,
                         TextureMapping.singleSlot(TextureSlot.LAYER0, new Material(crop.getTextures().getEssenceTexture())),
                         itemModels.modelOutput
                 );
             }
 
             if (crop.shouldRegisterSeedsItem()) {
+                var item = crop.getSeedsItem();
+
+                itemModels.itemModelOutput.register(
+                        item,
+                        new ClientItem(
+                                ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(item)),
+                                ClientItem.Properties.DEFAULT
+                        )
+                );
+
                 ModelTemplates.FLAT_ITEM.create(
-                        crop.getSeedsItem(),
+                        item,
                         TextureMapping.singleSlot(TextureSlot.LAYER0, new Material(crop.getTextures().getSeedTexture())),
                         itemModels.modelOutput
                 );
@@ -48,7 +70,18 @@ public class ItemModelJsonGenerator extends ModelProvider {
             var template = ModelTemplates.createItem(MysticalAgriculture.resource("augment").toString());
 
             for (var augment : AugmentRegistry.getInstance().getAugments()) {
-                itemModels.modelOutput.accept(ModelLocationUtils.getModelLocation(augment.getItem()), () -> template.createBaseTemplate(
+                var item = augment.getItem();
+                var location = ModelLocationUtils.getModelLocation(item);
+
+                itemModels.itemModelOutput.register(
+                        item,
+                        new ClientItem(
+                                ItemModelUtils.plainModel(location),
+                                ClientItem.Properties.DEFAULT
+                        )
+                );
+
+                itemModels.modelOutput.accept(location, () -> template.createBaseTemplate(
                         MysticalAgriculture.resource(augment.getNameWithSuffix("augment")),
                         Map.of()
                 ));
